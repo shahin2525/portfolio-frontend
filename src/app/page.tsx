e2c9e475f099df24b0c450e247a757c4a8004430
shahin2525 <shahin451999@gmail.com>
2025-04-30 13:14:11 +0530

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ const page = () => {
   return (
     <div>
       <Button className="text-red-400">Main Button</Button>
+      <Button className="text-green-300">Main Button</Button>
+      <Button className="text-yellow-400">Main Button</Button>
     </div>
   );
 };
